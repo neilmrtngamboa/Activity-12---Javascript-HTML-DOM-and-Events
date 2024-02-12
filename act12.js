33be7c784c11a195efc5c846aa5e55b54e338606
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-02-12 16:19:33 +0800

    added styling

--- act12.js
+++ act12.js
@@ -2,10 +2,10 @@ document.querySelector('#addTask').addEventListener('click', () => {
     
     if (document.querySelector('#todoInput').value != ""){
         document.querySelector('#taskList').innerHTML += 
-        `<div>
-        <button class="doneButton">Done</button>
-        <button class ="removeButton">Remove</button>
-        <span>${document.querySelector('#todoInput').value}</span>
+        `<div class = "mt-3">
+        <button class="btn btn-success rounded-circle doneButton">✔</button>
+        <button class ="btn btn-danger rounded-circle removeButton">X</button>
+        <span class="fw-bold fs-4">${document.querySelector('#todoInput').value}</span>
         </div>`;
         document.querySelector('#todoInput').value = "";
     }else{
